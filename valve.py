f4ec93d570eef57e0cfcf706721ba2caa0565aa2
hoogerheide <david.hoogerheide@nist.gov>
2023-08-19 14:09:48 -0400

    valves validate moves before attempting them

--- valve.py
+++ valve.py
@@ -69,15 +69,33 @@ class ValveBase(ComponentBase):
         
         self._portmap: dict[int, int] = {}
 
-    def move(self, position) -> None:
+    def move(self, position: int) -> None:
+        """Move valve to position
+
+        Args:
+            position (int): desired position
+        """
+        if self.validate_move(position):
+            self.position = position
+            self.update_map()
+            self.clear_connections()
+            self.update_connections()
+    
+    def validate_move(self, position: int) -> bool:
+        """Validate a move to a desired position
+
+        Args:
+            position (int): desired position
+
+        Returns:
+            bool: True if valid, False if not
+        """
+
         if position not in range(0, self.n_positions + 1):
-            logging.error(f'Requested position {position} is not an integer between 0 (off) and {self.n_positions}')
-            return
+            logging.error(f'{self.name} Move validation error: requested position {position} is not an integer between 0 (off) and {self.n_positions}')
+            return False
         
-        self.position = position
-        self.update_map()
-        self.clear_connections()
-        self.update_connections()
+        return True
 
 class DistributionValve(ValveBase):
     """Distribution valve representation. "Position" corresponds to the port number of the outlet port.
