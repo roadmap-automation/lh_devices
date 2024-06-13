8d38d52c076b7cfaa484f654d7b86623a385244f
David P. Hoogerheide <david.hoogerheide@nist.gov>
2024-06-13 17:56:27 -0400

    Recovery from wrong acknowledgement character received error (occurs when written character is dropped or response is too slow)

--- gsioc.py
+++ gsioc.py
@@ -74,16 +74,19 @@ class GSIOC(aioserial.AioSerial):
         if not self.is_open:
             self.open()
 
+        # initialize recovery data
+        recovery_data: dict | None = None
+
         # infinite loop to wait for a command. Break by cancelling the task
         try:
             while True:
 
-                logging.debug('Waiting for connection...')
+                logging.debug('GSIOC: Waiting for connection...')
 
                 await self.wait_for_connection()
 
                 if self.connected: # address received
-                    logging.debug('Connection established, waiting for command')
+                    logging.debug('GSIOC: Connection established, waiting for command')
                     # waits for a command
                     cmd = await self.wait_for_command()
 
@@ -96,23 +99,43 @@ class GSIOC(aioserial.AioSerial):
                             await self.send(self.gsioc_name)
                         
                         else:
+
                             # parses received command into a GSIOCMessage
                             data = await self.parse_command(cmd)
-
-                            # put message in listener queue if a client is connected; otherwise, send an error for immediate commands
-                            if self.client_lock.locked():
-                                await self.message_queue.put(data)
-
-                            if data.messagetype == GSIOCCommandType.IMMEDIATE:
-                                if self.client_lock.locked():
-                                    logging.debug('Waiting for response to immediate command')
-                                    response: str = await self.response_queue.get()
+                            
+                            # if previous send failed
+                            if recovery_data is not None:
+                                # if we're handling a repeated command, use recovery data, otherwise proceed as normal
+                                if recovery_data['command'] == cmd:
+                                    logging.debug(f"GSIOC: Using recovery data for repeat command {recovery_data['command']} with response {recovery_data['response']}")
+                                    response = recovery_data['response']                                
                                 else:
-                                    response: str = f'GSIOC error: no client connected on address {self.address}'
+                                    recovery_data = None
+                            
+                            # normal requests OR if previous send failed and it is not a repeat command
+                            if recovery_data is None:
 
-                                await self.send(response)
+                                # put message in listener queue if a client is connected; otherwise, send an error for immediate commands
+                                if self.client_lock.locked():
+                                    await self.message_queue.put(data)
 
-                logging.debug('Connection reset...')
+                                if data.messagetype == GSIOCCommandType.IMMEDIATE:
+                                    if self.client_lock.locked():
+                                        logging.debug('GSIOC: Waiting for response to immediate command')
+                                        response: str = await self.response_queue.get()
+                                    else:
+                                        response: str = f'GSIOC error: no client connected on address {self.address}'
+
+                            # attempt to send and save recovery data if not
+                            if data.messagetype == GSIOCCommandType.IMMEDIATE:
+                                if await self.send(response):
+                                    recovery_data = None
+                                else:
+                                    recovery_data = {'command': cmd,
+                                                    'response': response}
+
+
+                logging.debug('GSIOC: Connection reset...')
         except asyncio.CancelledError:
             logging.info('Closing GSIOC connection...')
             #await self.write1(chr(10))
@@ -137,13 +160,13 @@ class GSIOC(aioserial.AioSerial):
         # if address matches, echo the address
         if comm == self.address + 128:
             await self.write1(chr(self.address + 128))
-            logging.debug(f'address matches, writing {chr(self.address + 128).encode()}')
+            logging.debug(f'GSIOC: address matches, writing {chr(self.address + 128).encode()}')
             self.connected = True
         
         # if result is byte 255, send a break character
         # TODO: check if this should be self.ser.send_break
         elif comm == 255:
-            logging.debug('sending break')
+            logging.debug('GSIOC: sending break')
             await self.write1(chr(10))
             self.connected = False
 
@@ -158,7 +181,7 @@ class GSIOC(aioserial.AioSerial):
 
             # buffered command...read until line feed is sent
             if comm == 10:
-                logging.debug(f'got LF, starting buffered command read')
+                logging.debug(f'GSIOC: got LF, starting buffered command read')
                 await self.write1(chr(comm))
                 msg = ''
                 while comm != 13:
@@ -170,7 +193,7 @@ class GSIOC(aioserial.AioSerial):
 
                     msg += chr(comm)
 
-                logging.debug(f'got CR, end of message: {msg}')
+                logging.debug(f'GSIOC: got CR, end of message: {msg}')
 
                 return msg
             
@@ -194,7 +217,7 @@ class GSIOC(aioserial.AioSerial):
             cmd = cmd[:-1]
 
             # TODO: do stuff
-            logging.debug(f'Buffered command received: {cmd}')
+            logging.debug(f'GSIOC: Buffered command received: {cmd}')
             return GSIOCMessage(GSIOCCommandType.BUFFERED, cmd)
 
             #await self.send(f'You sent me buffered command {cmd}')
@@ -209,19 +232,22 @@ class GSIOC(aioserial.AioSerial):
         comm = await self.read_async()
         if len(comm):
             #print(comm)
-            logging.debug(int(comm.hex(), base=16))#, len(comm), [ord(c) for c in comm])
+            logging.debug(f'GSIOC: received character {int(comm.hex(), base=16)}') #, len(comm), [ord(c) for c in comm])
             return int(comm.hex(), base=16)
         else:
             return None
 
-    async def read_ack(self):
+    async def read_ack(self) -> bool:
         """
         Waits for acknowledgement of a sent byte from the master
         """
         ret = await self.read1()
         if ret != 6:
-            logging.warning(f'Warning: wrong acknowledgement character received: {ret}; attempting to repair comms')
+            logging.warning(f'GSIOC: wrong acknowledgement character received: {ret}; attempting to repair comms')
             #await self.repair_comms()
+            return False
+
+        return True
 
     async def repair_comms(self):
         """
@@ -239,18 +265,25 @@ class GSIOC(aioserial.AioSerial):
         """
         await self.write_async(char.encode(encoding='latin-1'))
 
-    async def send(self, msg: str):
+    async def send(self, msg: str) -> dict | None:
         """
-        Sends a message to the serial port, one character at a time
+        Sends a message to the serial port, one character at a time. 
+
+        Returns True if successful, False if read acknowledgement error
         """
         for char in msg[:-1]:
+            logging.debug(f'GSIOC: sending character {char}')
             await self.write1(char)
-            await self.read_ack()
+            if not await self.read_ack():
+                logging.warning(f'GSIOC: Terminating send of message {msg} due to wrong acknowledgement character, waiting for new connection')
+                return False
 
         # last character gets sent with high bit (add ASCII 128)
         await self.write1(chr(ord(msg[-1]) + 128))
 
         logging.debug(f'{self.port} (GSIOC) => {msg}')
+
+        return True
 
 async def main():
 
