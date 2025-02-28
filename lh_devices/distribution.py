4eb05d5cb130ae9d9436884fff89e4c3707971f3
David P. Hoogerheide <david.hoogerheide@nist.gov>
2025-02-28 08:57:35 -0500

    add two-source distribution system

--- lh_devices/distribution.py
+++ lh_devices/distribution.py
@@ -21,3 +21,14 @@ class DistributionSingleValve(DistributionBase):
         super().__init__(distribution_valve.valve.n_positions, [distribution_valve], injection_port, name)
 
         self.modes = {str(i): Mode({distribution_valve: i}, final_node=distribution_valve.get_nodes()[i]) for i in range(self.n_positions + 1)}
+        self.modes.update({'Standby': Mode({distribution_valve: 0})})
+
+class DistributionSingleValveTwoSource(DistributionSingleValve):
+
+    def __init__(self, distribution_valve: ValvePositionerBase, source_valve: ValvePositionerBase, injection_port: InjectionPort, name='') -> None:
+        super().__init__(distribution_valve, injection_port, name)
+
+        self.modes.update({'LH': Mode({source_valve: 1}),
+                           'Rinse': Mode({source_valve: 3}),
+                           'Standby': Mode({source_valve: 0,
+                                            distribution_valve: 0})})
