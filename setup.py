5f27458688b6fe3f9f9c8a561a2bc94a85d9607d
David P. Hoogerheide <david.hoogerheide@nist.gov>
2025-07-16 11:41:29 -0400

    update setup.py to include templates, new url, all packages

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ setup(
     name='lh_devices',
     version='0.1.0',
     packages=['lh_devices',
+              'lh_devices.camera',
+              'lh_devices.gilson',
               'lh_devices.hamilton',
               'lh_devices.injection',
-              'lh_devices.qcmd'],
-    url='https://github.com/hoogerheide/lh_devices',
+              'lh_devices.qcmd',
+              'lh_devices.rinse'],
+    package_data={'lh_devices': ['templates/roadmap.html']},
+    url='https://github.com/roadmap-automation/lh_devices',
     license='Public Domain',
     author='David P. Hoogerheide',
     author_email='david.hoogerheide@nist.gov',
@@ -20,5 +24,5 @@ setup(
               "scipy",
               "plotly",
               "svg.py"
-    ]
+    ],
 )
